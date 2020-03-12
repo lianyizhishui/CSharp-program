f952d4ac78b39e07d0e886cdb24dac5364a14c11
lianyizhishui <49826378+lianyizhishui@users.noreply.github.com>
2020-03-12 21:46:00 +0800

    lesson2-task3

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace work2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 2, 4, 3, 0, 43, 45, 32, 242, 24 };
+
+            Console.WriteLine("最小" + array.Min());
+            Console.WriteLine("最大" + array.Max());
+            Console.WriteLine("平均" + array.Average());
+            Console.WriteLine("总合" + array.Average() * 10);
+
+            Console.Read();
+
+        }
+    }
+}
